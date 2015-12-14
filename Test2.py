432f924c1b0578fe3f9fe2999f6b5153426afff8
1009576562@qq.com <1009576562@qq.com>
2015-12-14 18:55:05 +0800

    1214

---
+++ Test2.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+import re
+import urllib
+
+
+def getHtml(url):
+    page = urllib.urlopen(url)
+    html = page.read()
+    return html
+
+
+def getImg(html):
+    reg = r'src="(.+?\.jpg)" pic_ext'
+    imgre = re.compile(reg)
+    imglist = re.findall(imgre, html)
+    x = 0
+    for imgurl in imglist:
+        urllib.urlretrieve(imgurl, 'f:\photoTest\%s.jpg' % x)
+        x += 1
+
+
+html = getHtml("http://tieba.baidu.com/p/2460150866")
+# 汉化
+print getImg(html)
---
+++ Test3.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+
+# print 2.0/3
+
+# # sentence=raw_input("Sentence：")
+# # screen_width=80
+# # text_width = len(sentence)
+# # box_width = text_width + 6
+# # left_margin = (screen_width - box_width) // 2
+# # print
+# # print ' ' * left_margin + '+' + '-' * (box_width - 4) + '+'
+# # print ' ' * left_margin +'| '+' '*text_width + ' |'
+# # print ' ' * left_margin +'| '+ sentence + ' |'
+# # print ' ' * left_margin +'| '+' '*text_width + ' |'
+# print ' ' * left_margin +'+' +'-'*(box_width - 4)+'+'
+# print
+
+# database=[['qwe','123'],['abc','123']]
+# username=raw_input('login name:')
+# pwd=raw_input('pwd:')
+# if [username,pwd] in database:
+#     print 'access successful'
+# else:
+#     print 'access false'
+
+print list("hello")
+name = [1, 23, 4]
+del name[1:2]
+print name
