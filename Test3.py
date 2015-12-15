cf3f4dc53218fa04b352d506a087ad432fed4732
1009576562@qq.com <1009576562@qq.com>
2015-12-15 18:09:26 +0800

    1215

--- Test3.py
+++ Test3.py
@@ -22,8 +22,16 @@
 #     print 'access successful'
 # else:
 #     print 'access false'
-
+import string
 print list("hello")
 name = [1, 23, 4]
 del name[1:2]
 print list(reversed([1, 2, 3]))
+print '%-*.*s' % (9, 2, '234.234')  # -表示左对齐，左*表示最小字符宽度，右*表示最大字符宽度（若为实数，则表示小数点后的位数）
+print eval(repr('12312324234'))
+print string.letters  # 包含所有字母（大小写）,string.lowercase小写字母字符串，string.uppercase大写字母字符串
+print string.digits  # 包含0-9的字符串
+print '所有可打印的字符：' + (string.printable)  # 所有可打印字符的字符串
+print (string.punctuation)  # 包含所有标点的字符串
+title = '我'
+print len(title)
---
+++ test4.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+base = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def dec2bin(string_num):
+    num = int(string_num)
+    mid = []
+    while True:
+        if num == 0: break
+        num, rem = divmod(num, 2)
+        mid.append(base[rem])
+
+    return ''.join([str(x) for x in mid[::-1]])
+
+
+print dec2bin(123)
+byte = bytearray(123)
+print byte
